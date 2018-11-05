1cbea12545e26359a35b9ee83027ab0b93635557
Triston J. Taylor <pc.wiz.tt@gmail.com>
2018-11-05 03:04:27 -0600

    Demo
    make
    
    dist/bin/jskit demo.js "some script" parameters

--- demo.js
+++ demo.js
@@ -2,13 +2,18 @@
 
 /* global system, parameter */
 
+// include("/some/javascript/file.js")
+
 setFileContent('js.out', parameter.join(", ") + '\n');
+// setFileContent.append(FILE, CONTENTS)
 
 var output = getFileContent('js.out');
 echo("file contents:", output);
 system('rm js.out');
 
 set('SCRIPT', parameter[0]);
+// set(VAR, VALUE, TRUE) // don't overwrite if set
+
 echo("environment variable:", get('SCRIPT'));
 clear('SCRIPT');
 echo(keys());
@@ -19,5 +24,8 @@ if (get('notavar') !== null) {
 
 system.write('cat', system.read('ls').output);
 
+// this doesn't exist yet
 echo("you typed: ", readLine("type something > "));
+
+// this doesn't exist yet
 exit(0);
